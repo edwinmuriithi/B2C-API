a0ec68e3bf255138bc015ccaba1b97ae1eae4b07
edwinmuriithi <kabuimuriithi@gmail.com>
2024-07-21 23:01:49 +0300

    feat(Controller): Create method to fetch payment status

--- src/main/java/com/example/B2C/API/controller/B2CController.java
+++ src/main/java/com/example/B2C/API/controller/B2CController.java
@@ -1,14 +1,14 @@
 package com.example.B2C.API.controller;
 
 import com.example.B2C.API.entities.GwRequests;
+import com.example.B2C.API.entities.Result;
 import com.example.B2C.API.services.B2CService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/b2c")
@@ -21,5 +21,11 @@ public class B2CController {
     public ResponseEntity<String> receiveB2CRequest(@Valid @RequestBody GwRequests gwRequest) {
         b2CService.processB2CRequest(gwRequest);
         return ResponseEntity.ok("B2C request received");
+    }
+
+    @GetMapping("/status/{id}")
+    public ResponseEntity<Result> fetchPaymentStatus(@PathVariable UUID id) {
+        Result result = b2CService.fetchPaymentStatus(id);
+        return ResponseEntity.ok(result);
     }
 }
