3affbd143d226de32888053c29ed868339dd492a
edwinmuriithi <kabuimuriithi@gmail.com>
2024-07-21 21:21:16 +0300

    Initial Commit

---
+++ src/main/java/com/example/B2C/API/B2CApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.B2C.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2CApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2CApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/B2C/API/B2CApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.B2C.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2CApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
