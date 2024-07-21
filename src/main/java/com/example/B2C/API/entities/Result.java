bcaf62752bb2fadbb60b84544578e622d5dc9eb8
edwinmuriithi <kabuimuriithi@gmail.com>
2024-07-21 22:04:07 +0300

    feat(Entity) Create Results model

---
+++ src/main/java/com/example/B2C/API/entities/Result.java
@@ -0,0 +1,12 @@
+package com.example.B2C.API.entities;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class Result {
+    private UUID id;
+    private String status;
+    private String ref;
+}
